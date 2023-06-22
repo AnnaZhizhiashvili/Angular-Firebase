a06e584a4d3c7c60897bee997714df0bf18c0bb0
AnnaZhizhiashvili <ana.zhizhiashvili@protonmail.com>
2023-06-22 15:45:46 +0400

    add ng-bootstrap dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import {environment} from "../environments/environment.development";
 import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import {connectDatabaseEmulator, getDatabase, provideDatabase} from "@angular/fire/database";
+import { getDatabase, provideDatabase} from "@angular/fire/database";
 import {NavbarComponent} from "./navbar/navbar.component";
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
@@ -17,6 +17,7 @@ import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterLink} from "@angular/router";
+import { RouterLink } from "@angular/router";
+import { NgbDropdownModule } from "@ng-bootstrap/ng-bootstrap"
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, NgbDropdownModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
